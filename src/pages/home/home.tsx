8c46ec6a8555b7c45439d13137ee74617ab5be42
AB0529 <supercrab115@gmail.com>
2022-03-18 03:34:18 -0400

    Added loadable components

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,16 @@
-import About from "./components/sections/about/about";
-import Banner from "./components/sections/banner/banner";
-import Projects from "./components/sections/projects/projects";
-import Footer from "./components/sections/footer/footer";
-import Navbar from "./components/nav/navbar";
-import { useEffect } from "react";
+// import About from "./components/sections/about/about";
+// import Banner from "./components/sections/banner/banner";
+// import Projects from "./components/sections/projects/projects";
+// import Footer from "./components/sections/footer/footer";
+// import Navbar from "./components/nav/navbar";
+
+import loadable from "@loadable/component";
+
+const Navbar = loadable(() => import('./components/nav/navbar'))
+const Banner = loadable(() => import('./components/sections/banner/banner'))
+const About = loadable(() => import('./components/sections/about/about'))
+const Projects = loadable(() => import('./components/sections/projects/projects'))
+const Footer = loadable(() => import('./components/sections/footer/footer'))
 
 const HomePage = () => {
     return (
