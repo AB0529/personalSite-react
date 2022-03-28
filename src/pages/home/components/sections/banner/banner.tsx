4bd035eff648442b83fdcb15f3c6b4986ad1f393
AB0529 <supercrab115@gmail.com>
2022-03-28 02:45:01 -0400

    Fixed banner text scroll overlap

--- src/pages/home/components/sections/banner/banner.tsx
+++ src/pages/home/components/sections/banner/banner.tsx
@@ -1,28 +1,29 @@
+import { useEffect, useRef, useState } from "react"
 import { Container } from "react-bootstrap"
-import { Parallax, ParallaxProvider } from "react-scroll-parallax"
-import Navbar from "../../nav/navbar"
+import { Parallax, ParallaxBanner, ParallaxProvider } from "react-scroll-parallax"
 
 import "../sections.scss"
 import "./banner.scss"
 
 export default () => {
-
     return (
         <>
             <ParallaxProvider>
-                <Container id="#home" fluid className="banner-section">
-                    <div className="d-flex align-items-center justify-content-center">
-                        <Parallax speed={-20}>
-                            <div className="banner-title">
-                                <span id="bracs"> &lt; </span>
-                                <span id="first">Anish</span>
-                                <span> </span>
-                                <span id="last">B.</span>
-                                <span id="bracs"> /&gt; </span>
-                            </div>
-                        </Parallax>
-                    </div>
-                </Container>
+                <ParallaxBanner>
+                    <Container id="#home" fluid className="banner-section">
+                        <div className="d-flex align-items-center justify-content-center">
+                            <Parallax speed={-25}>
+                                <div className="banner-title">
+                                    <span id="bracs"> &lt; </span>
+                                    <span id="first">Anish</span>
+                                    <span> </span>
+                                    <span id="last">B.</span>
+                                    <span id="bracs"> /&gt; </span>
+                                </div>
+                            </Parallax>
+                        </div>
+                    </Container>
+                </ParallaxBanner>
             </ParallaxProvider>
         </>
     )
